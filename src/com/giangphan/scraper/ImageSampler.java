4c1485d088f3779db42d89cb3c4f012766fec6f7
Giang Phan <john.phan@gmail.com>
2012-03-15 20:35:19 -0700

    Adding bejeweler main class that ties it all together

--- src/com/giangphan/scraper/ImageSampler.java
+++ src/com/giangphan/scraper/ImageSampler.java
@@ -3,6 +3,11 @@ package com.giangphan.scraper;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 
+/**
+ * Samples a buffered image and returns an average sample depending on the grid width and height.
+ * @author gphan
+ *
+ */
 public class ImageSampler {
 	private BufferedImage image;
 	private final int gridWidth;
@@ -27,13 +32,44 @@ public class ImageSampler {
 	 * @return
 	 */
 	public Color[][] getAverageColors() {
-		int cellWidth = this.image.getWidth() / this.gridWidth;
-		int cellHeight = this.image.getHeight() / this.gridHeight;
+		Color[][] colors = new Color[gridWidth][gridHeight];
 		
-
+		for (int i = 0; i < gridWidth; i++) {
+			for (int j = 0; j < gridHeight; j++) {
+				colors[i][j] = getAverageColor(i, j);
+			}
+		}
+		
+		return colors;
 	}
 	
 	private Color getAverageColor(int gridx, int gridy) {
-		int cellWidth = this.image.getWidth() / this.gridWidth;
+		int totalPixels = cellWidth * cellHeight;
+		int[] pixelRgb = this.image.getRGB(
+				gridx * cellWidth, 
+				gridy * cellHeight, 
+				cellWidth, 
+				cellHeight, 
+				null, 0, 0);
+		int[] avg = new int[] { 0, 0, 0 };		
+		
+		assert(totalPixels == pixelRgb.length);
+		
+		for (int i = 0; i < pixelRgb.length; i++) {
+			int pixel = pixelRgb[i];
+			int red = (pixel >> 16) & 0xff;
+			int green = (pixel >> 8) & 0xff;
+			int blue = (pixel & 0xff);
+			
+			avg[0] += red;
+			avg[1] += green;
+			avg[2] += blue;
+		}
+		
+		avg[0] /= totalPixels;
+		avg[1] /= totalPixels;
+		avg[2] /= totalPixels;
+		// Create new color
+		return new Color(avg[0], avg[1], avg[2]);
 	}
 }
